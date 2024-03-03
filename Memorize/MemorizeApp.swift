9f6b353c10da70f819d51b7dfb94aaa6f902a764
wy471x <wy471x@gmail.com>
2024-03-03 18:11:11 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 万勇 on 2024/3/3.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
